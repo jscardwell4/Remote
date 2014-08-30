2fd482d53fd9a4d67d006eb411bab3446b3a60fe
Jason Cardwell <cardwell.jason@gmail.com>
2014-08-30 09:28:12 -0700

    Adding ParseKit back
    Signed-off-by: Jason Cardwell <cardwell.jason@gmail.com>

---
+++ MSKit/ParseKit/include/ParseKit/PKCompositeParser.h
@@ -0,0 +1,19 @@
+//
+//  PKCompositeParser.h
+//  ParseKit
+//
+//  Created by Todd Ditchendorf on 10/5/12.
+//
+//
+
+#import "PKParser.h"
+@interface PKCompositeParser : PKParser {
+    
+}
+
+/*!
+    @brief      Adds a parser to the composite.
+    @param      p parser to add
+*/
+- (void)add:(PKParser *)p;
+@end
---
+++ MSKit/ParseKit/src/nodes/PKCompositeNode.m
@@ -0,0 +1,48 @@
+//
+//  PKCompositeNode.m
+//  ParseKit
+//
+//  Created by Todd Ditchendorf on 10/5/12.
+//
+//
+
+#import "PKCompositeNode.h"
+#import "ParseKit_Internal.h"
+static NSDictionary *sClassTab = nil;
+
+@implementation PKCompositeNode
+
++ (void)initialize {
+    if ([PKCompositeNode class] == self) {
+        sClassTab = [@{
+            @"*" : [PKRepetition class],
+            @"~" : [PKNegation class],
+            @"-" : [PKDifference class],
+        } retain];
+    }
+}
+
+
+- (NSUInteger)type {
+    return PKNodeTypeComposite;
+}
+
+
+- (void)visit:(id <PKNodeVisitor>)v; {
+    [v visitComposite:self];
+}
+
+
+- (Class)parserClass {
+    NSString *typeName = self.token.stringValue;
+    Class cls = sClassTab[typeName];
+    
+    return cls;
+}
+
+
+- (BOOL)isTerminal {
+    return NO;
+}
+
+@end
